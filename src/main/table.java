b537d8e795b56d3930068e75376d5fcad8c8f5d5
clewarne14 <clewarne15@gmail.com>
2021-04-13 10:52:59 -0700

    created a constructor and a played array list for the card pile

--- src/main/table.java
+++ src/main/table.java
@@ -1,5 +1,16 @@
 package main;
 
+import java.util.*;
+
 public class table{
+    private Stack<card> played;
+
+
+    public table(){
+        played = new Stack<card>();
+    }
+    public Stack<card> getPlayed(){
+        return played;
+    }
     
 }
