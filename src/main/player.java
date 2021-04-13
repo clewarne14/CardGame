47ec6e843c12deae02e7c88311449f63a47d8a4a
clewarne14 <clewarne15@gmail.com>
2021-04-13 10:52:19 -0700

    created getter methods and three array lists of cards

--- src/main/player.java
+++ src/main/player.java
@@ -1,7 +1,32 @@
 package main;
 
-
+import java.util.*;
 
 public class player{
-    
+    private ArrayList<card> hand;
+    private ArrayList<card> faceDown;
+    private ArrayList<card> faceUp;
+    private int playerUID;
+
+    public player(int playerUIDTemp){
+        hand = new ArrayList<card>();
+        faceDown = new ArrayList<card>();
+        faceUp = new ArrayList<card>();
+        playerUID = playerUIDTemp;
+    }
+
+
+
+    public ArrayList<card> getHand(){
+        return hand;
+    }
+    public ArrayList<card> getFaceDown(){
+        return faceDown;
+    }
+    public ArrayList<card> getFaceUp(){
+        return faceUp;
+    }
+    public int getPlayerUID(){
+        return playerUID;
+    }
 }
