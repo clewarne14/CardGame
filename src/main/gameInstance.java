e9dbfae438d91a8aad68afefb8aa53af254d3cde
clewarne14 <clewarne15@gmail.com>
2021-04-12 17:40:58 -0700

    added a way to play a card and get the deck instance

--- src/main/gameInstance.java
+++ src/main/gameInstance.java
@@ -15,5 +15,18 @@ public class gameInstance{
         deck = new deck();
     }
 
+    public boolean playCard(card top, card played){
+        if(played.getNumber() >= top.getNumber()){
+            return true;
+        } else if (played.getNumber() == 10 || played.getNumber() == 2 || played.getNumber() == 1){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public deck getDeck(){
+        return deck;
+    }
+
     
 }
